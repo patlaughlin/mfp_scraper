bf7fad6e1131422dcd14d0b3a266a5228b7b278c
Patrick Laughlin <patrick.laughlin@levvel.io>
2019-04-09 08:47:03 -0400

    wrap up the initial pass at the tdee calculation

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var parse = require('csv-parse/lib/sync');
 const moment = require('moment');
 
 // Day i started my diet to another year in the future.
-var url = 'http://www.myfitnesspal.com/reports/printable_diary/prlaugh?from=2019-02-16&to=2020-01-23';
+var url = 'http://www.myfitnesspal.com/reports/printable_diary/prlaugh?from=2019-01-01&to=2020-01-01';
 
 var user = new User({
     firstName: 'Patrick',
@@ -57,58 +57,64 @@ request(url, function (error, response, html) {
         const gProtein = parseInt($total.next().next().next().text(), 10);
         const totalInt = parseInt(total.replace(/,/g, ''), 10);
 
-        const realCalories = ((gCarbs * 4) + (gFat * 9) + (gProtein * 4));
+        let realCalories = ((gCarbs * 4) + (gFat * 9) + (gProtein * 4));
 
         // console.log(`${dateString}: ${gFat}, ${gCarbs}, ${gProtein}`);
         // console.log(`${dateString}: ${realCalories}`);
-        /**
-         * Anything below 1300 is an obvious logging error for me
-         */
-
         const weightObj = _.find(weightCols, ['Date', date]);
+        if (realCalories <= 1600) {
+            realCalories = 2400;
+        }
         _.set(weightObj, 'Calories', realCalories);
+        _.set(weightObj, 'Recorded', parseFloat(weightObj.Recorded));
+        _.unset(weightObj, 'Moving Average');
 
         updatedWeights.push(weightObj);
-
-        // if (totalInt >= 1500) {
-        //     totals.push({
-        //         date: dateString,
-        //         total: realCalories,
-        //         weight: null
-        //     });
-        // }
     });
 
-    const weightsByWeek = _.groupBy(updatedWeights, (result) => moment(result['Date'], 'YYYY-MM-DD').startOf('isoWeek'));
-    console.log(weightsByWeek);
 
     /**
-     * Add corresponding date to totals dictionary
+     * Begin TDEE work
+     * @param startDate
+     * @param weights
+     * calories_consumed_in_a_week + (((-weight_lost_from_start) * calories_in_pound) / days_in_week))
      */
-    totals.forEach(el => {
-        el.weight = weights[el.date];
-    });
-
-    /**
-     * pluck out any days with unlogged weight
-     */
-    _.remove(totals, {weight: undefined});
-
-    var totalsChunkedByWeek = _.chunk(totals, 7);
-    var averages = [];
-
-
-    totalsChunkedByWeek.forEach(function (week) {
-        averages.push({
-            weekOf: week[0].date,
-            calories: _.meanBy(week, 'total'),
-            weight: _.meanBy(week, 'weight'),
-            weightLossRate: null
+    function getTDEE(startDate, weights) {
+        const filteredWeights = _.dropWhile(weights, weight => {
+            return moment(weight.Date).isBefore(startDate)
         });
-    });
+        const startWeight = _.find(filteredWeights, ['Date', startDate]).Recorded;
+        const dataByWeek = _.groupBy(filteredWeights, (result) => moment(result['Date'], 'YYYY-MM-DD').startOf('isoWeek'));
+        /**
+         * create a collection with average calories in a week, weight lost from start, and the date of that week
+         */
+        let meanData = _.map(dataByWeek, (week, index) => {
+            const weight = _.meanBy(week, 'Recorded');
+            const calories = _.round(_.meanBy(week, 'Calories'), 0);
+            let weightDiff = 0;
+            if (index === 0) {
+                weightDiff = _.round(weight - startWeight, 2);
+            }
 
+            return {date: _.head(week).Date, weight, weightDiff, calories, rowCount: _.size(week)};
+        });
 
-    updateCurrentWeightLossRate(averages);
+        meanData = _.map(meanData, (data, index) => {
+            const weightDiff = index === 0 ? data.weightDiff : _.round(data.weight - meanData[index - 1].weight, 2);
+            return {...data, weightDiff};
+        });
+
+        meanData = meanData.map(data => {
+            const TDEE = data.calories + (((-data.weightDiff) * 3500) / data.rowCount);
+            return {...data, TDEE}
+        });
+
+        return _.round(_.meanBy(meanData, 'TDEE'), 0);
+    }
+
+    function getTargetCalories(TDEE, goalWeightDiffPerWeek, isLoss = true) {
+        return TDEE - (goalWeightDiffPerWeek * 3500) / 7;
+    }
 
 
     function calculateWeightLossMacros(currentWeight, calories, user) {
@@ -123,48 +129,9 @@ request(url, function (error, response, html) {
         user.fatGrams = gFat.toFixed(2);
     }
 
-
-    /**
-     * get the averages of the averages for determining final TDEE
-     * and current average weightloss
-     */
-    /**
-     * this function does TOO MUCH!
-     * @param averages
-     * @param desiredWeightlossPerWeek
-     * @returns {{TDEE: *, currentWeightLossRate: *, recommendedCalories: number}}
-     */
-    function calculateGoals(averages, desiredWeightlossPerWeek, user) {
-        const CALORIES_IN_FAT = 3500;
-        var averageCalories = _.meanBy(averages, 'calories');
-        var currentWeightLossRate = _.meanBy(averages, 'weightLossRate');
-        var currentDeficit = (CALORIES_IN_FAT * currentWeightLossRate) / 7;
-        var desiredDeficit = (CALORIES_IN_FAT * desiredWeightlossPerWeek) / 7;
-        var deficitDiff = desiredDeficit - currentDeficit; // i'm not totally sure if we need this?
-        var TDEE = averageCalories + currentDeficit;
-        var recommendedCalories = TDEE - desiredDeficit;
-
-        user.TDEE = parseInt(TDEE, 10);
-        user.currentWeightLossRate = currentWeightLossRate.toFixed(2);
-        user.recommendedCalories = parseInt(recommendedCalories, 10);
-    }
-
-
-    calculateGoals(averages, user.desiredWeightLossRate, user);
-    calculateWeightLossMacros(_.findLast(weights), user.recommendedCalories, user);
-    user.totalWeightLost = Calculator.getTotalWeightLost(averages, weights, user);
+    const myTDEE = getTDEE('2019-03-20', updatedWeights);
+    const targetCalories = getTargetCalories(myTDEE, 1);
+    console.log(`TDEE: ${myTDEE}`);
+    console.log(`You should eat: ${targetCalories} calories per day`);
 });
-//
-// var restify = require('restify');
-//
-// function respond(req, res, next) {
-//   res.send(user);
-//   next();
-// }
-//
-// var server = restify.createServer();
-// server.get('/users/:name', respond);
-//
-// server.listen(8080, function() {
-//   console.log('%s listening at %s', server.name, server.url);
-// });
+
